2066f9d9edb00f1c98047b320801b2a147c6d1c3
huanghuo <Hhjzrhuy19950824>
2024-02-05 22:44:01 +0800

    x

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // On Chrome Install
-// chrome.runtime.onInstalled.addListener(function (details) {
-//     if (details.reason === "install") {
-//         chrome.tabs.create({ url: "https://chat.openai.com/auth/login" });
-//     }
-// });
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install") {
+        chrome.tabs.create({ url: "https://chat.openai.com/auth/login" });
+    }
+});
